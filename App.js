1dd4e2f27654565e1014857f7525bb17e7379ae7
alexva2002 <shivam25oct2002@gmail.com>
2024-04-02 00:55:16 +0530

    Episode-02

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
-/**
- * <div id="parent">
- *     <div id="child">
- *            <h1> I'm h1 tag</h1>
- *            <h1> I'm h2 tag</h1>
- *     </div>
- *
- *
- * </div>
- *
- *ReactElement(Object)==>HTML(bowser Understands)
- *
- */
+import React from "react";
+import ReactDOM from "react-dom";
+
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I'm an h1 tag."),
-    React.createElement("h2", {}, "I'm an h2 tag."),
+    React.createElement("h1", {}, "This is Namaste react 🚀"),
+    React.createElement("h2", {}, "by Shivam Maddheshiya"),
   ]),
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "I'm an h1 tag."),
@@ -23,18 +14,10 @@ const parent = React.createElement("div", { id: "parent" }, [
 ]);
 
 
-//JSX
 
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading", xyz: "abc" },
-//   "Hello World from React"
-// );
-
-// console.log(heading); // This will return an object to you.
-console.log(parent);
+console.log(parent);  //object
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// root.render(heading);
+
 root.render(parent);
---
+++ README.md
@@ -0,0 +1,26 @@
+# Namaste-React
+Your code goes here.
+
+NPM--npm is every thing but not Note Packege Manager.
+
+
+
+# parcel
+ - Dev Build
+ - Local Server
+ - HMR = Hot Module Replacement 
+ - File Watching Algorithm - written in C++
+ - Caching - Faster Builds
+ - Image Optimization
+ - Minification
+ - Bundings
+ - Compressing
+ - Consistant Hashing
+ - Code Splitting
+ - Differential Bundling to support older browser
+ - Diagnostic 
+ - Error Handling
+ - https
+ - Tree Shaking(Algorithm) remove unused code for you.
+ - Different dev and prod. bundles
+ -  
